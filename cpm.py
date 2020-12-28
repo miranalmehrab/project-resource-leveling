10d17b1d0a64da788919f94b2aea1cdec8a3391e
MiranAlMehrab <bsse0931@iit.du.ac.bd>
2020-12-28 16:05:18 +0600

    backward pass and slack time are added

--- cpm.py
+++ cpm.py
@@ -3,7 +3,6 @@ id_name_pair = []
 
 def get_data_from_input_file():
     global node_matrix
-    global id_name_pair
 
     node_counter = 1
     fp = open('input2.txt', 'r')
@@ -15,6 +14,7 @@ def get_data_from_input_file():
         node['id'] = node_counter 
         node['name'] = comma_splitted_line[0]
         node['predecessor'] = comma_splitted_line[1].strip().split(';')
+        node['ancestor'] = []
         node['duration'] = comma_splitted_line[2].strip()
         # node['resource'] = comma_splitted_line[3].strip()
         node['slack'] = 0
@@ -32,7 +32,13 @@ def get_data_from_input_file():
         id_name_pair.append({'id':node['id'], 'name':node['name']})
         node_counter += 1
 
+def print_node_matrix():
+    global node_matrix
 
+    print('name  ES   EF  LS  LF  Sl')
+    for node in node_matrix:
+        print(node['name']+ '   '+str(node['ES'])+ '   '+str(node['EF'])+'   '+str(node['LS'])+'    '+str(node['LF'])+'    '+str(node['slack']))
+        
 
 def check_if_fp_is_true_for_all_predecessors(predecessors):
     for predecessor in predecessors:
@@ -44,32 +50,27 @@ def check_if_fp_is_true_for_all_predecessors(predecessors):
 
 def get_max_predecessor_ef_value(predecessors):
     predecessor_ef_values = []
-    
+
     for predecessor in predecessors:
         for node in node_matrix:
             if node['name'] == predecessor and node['FP'] is True:
                 predecessor_ef_values.append(node['EF'])
     
-    # print(predecessor_ef_values)
     return max(predecessor_ef_values)
 
 
 def calculate_start_node():
     global node_matrix
-
     for node in node_matrix:
         if node['predecessor'] == ['']:
             node['ES'] = 0
             node['EF'] = node['duration']
             node['FP'] = True
-            
-            print(node)
-            print('gotcha')
+    
 
 def forward_pass_of_the_network():
     global node_matrix
     global id_name_pair
-    # print_node_matrix()
 
     node_matrix.sort(reverse = False, key = lambda x: len(x['predecessor']))
 
@@ -79,10 +80,7 @@ def forward_pass_of_the_network():
                 node['ES'] = get_max_predecessor_ef_value(node['predecessor']) 
                 node['EF'] = int(node['ES']) + int(node['duration'])
                 node['FP'] = True
-            
-                
-            # print(str(node['id'])+ ' '+node['name']+ ' '+str(node['ES'])+ ' '+str(node['EF'])+' '+str(node['FP']))
-        
+
         node_counter = 0
         for node in node_matrix:
             if node['FP'] is True:
@@ -92,21 +90,87 @@ def forward_pass_of_the_network():
             break
 
     
-    # print(id_name_pair)
+
+def find_ancestors_of_node():
+    global node_matrix
+
+    for predecessor_node in node_matrix:
+        for ancestor_node in node_matrix:
+            if predecessor_node['name'] in ancestor_node['predecessor']:
+                predecessor_node['ancestor'].append(ancestor_node['name'])
+    
+
+def check_if_bp_is_true_for_all_ancestors(ancestors):
+    global node_matrix
+
+    for ancestor in ancestors:
+        for node in node_matrix: 
+            if ancestor == node['name'] and node['BP'] is False:
+                return False
+    return True
+
+
+def get_min_ancestor_ls_value(ancestors):
+    ancestor_ls_values = []
+
+    for ancestor in ancestors:
+        for node in node_matrix:
+            if ancestor == node['name'] and node['BP'] is True:
+                ancestor_ls_values.append(node['LS'])
+    
+    print(ancestor_ls_values)
+    return min(ancestor_ls_values)
+
 
 def backward_pass_of_the_network():
-    pass
+    global node_matrix
+    node_matrix.sort(reverse = True, key = lambda x: int(x['EF']))
+    last_ef_of_project = node_matrix[0]['EF']
 
-def print_node_matrix():
+    node_matrix.sort(key = lambda x:x['ancestor'])
+    while True:
+        for node in node_matrix:
+            if node['BP'] is False:
+                
+                if len(node['ancestor']) == 0:
+                    node['LF'] = int(last_ef_of_project)
+                    node['LS'] = int(node['LF']) - int(node['duration'])
+                    node['BP'] = True
+            
+                elif (check_if_bp_is_true_for_all_ancestors(node['ancestor'])):
+                    node['LF'] = int(get_min_ancestor_ls_value(node['ancestor']))
+                    node['LS'] = int(node['LF']) - int(node['duration'])
+                    node['BP'] = True
+
+
+        print(node['name']+ ' '+str(node['ES'])+ ' '+str(node['EF'])+' '+str(node['LS'])+' '+str(node['LF']))
+        print(node['ancestor'])
+        print('')
+ 
+        node_counter = 0
+        for node in node_matrix:
+            if node['BP'] is True:
+                node_counter += 1
+        
+        if node_counter == len(node_matrix):
+            break
+
+def calculate_slack_time_of_the_nodes():
     global node_matrix
     for node in node_matrix:
-        print(str(node['id'])+ ' '+node['name']+ ' '+str(node['ES'])+ ' '+str(node['EF']))
-    
+        node['slack'] = int(node['LS']) - int(node['ES'])    
+
 
 def main():
     get_data_from_input_file()
+    
     calculate_start_node()
     forward_pass_of_the_network()
+    
+    find_ancestors_of_node()
+    backward_pass_of_the_network()
+    
+    calculate_slack_time_of_the_nodes()
     print_node_matrix()
 
 if __name__ == "__main__":
