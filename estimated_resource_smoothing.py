3f761751b640e647ff6a6415e07cb45b0d744711
A. N. TANVIR <35560149+antanvir@users.noreply.github.com>
2020-12-29 21:10:34 +0600

    estimated approach too much optimal hoye gese

--- estimated_resource_smoothing.py
+++ estimated_resource_smoothing.py
@@ -8,7 +8,6 @@ class EstimatedResourceSmoothing:
 
     def __init__(self, node_matrix=[]):
         self.node_matrix = node_matrix
-        # print('- Node_Matrix -\n', self.node_matrix)
         self.critical_activities = []
         self.critical_activities_length = 0
         self.nonCritical_activities = {}
@@ -19,7 +18,9 @@ class EstimatedResourceSmoothing:
         self.R2_by_time = []
         self.optimal_total_R = int(1e9)
         self.optimal_total_R_square = int(1e9)
+        # print('- Node_Matrix -\n', self.node_matrix)
     
+
 
     def print_estimate_schedule_details(self):
         print("---------------------------------")
@@ -37,11 +38,13 @@ class EstimatedResourceSmoothing:
                 print(node["name"], "\t", node["OS"], "\t", node["OF"], "\t", node["slack"])
 
 
+
     def separate_critical_activities(self):
         for node in self.node_matrix:
             if node["critical"] == True:
                 self.project_duration += int(node["duration"])
                 self.critical_activities.append(node)
+
 
 
     def generate_time_resource_matrix(self):
@@ -59,6 +62,7 @@ class EstimatedResourceSmoothing:
         return time_resource_matrix
 
 
+
     def update_optimal_start_and_finish_time(self, comb_choice, pos_in_combination_and_node_matrix_ind_mapping):
         for i, shift in enumerate(comb_choice):
             node_matrix_index = pos_in_combination_and_node_matrix_ind_mapping[i]
@@ -67,15 +71,20 @@ class EstimatedResourceSmoothing:
             self.node_matrix[node_matrix_index]["OF"] = es + int(shift) + duration
 
 
+
+
     def check_for_optimality(self, time_resource_matrix, comb_choice, pos_in_combination_and_node_matrix_ind_mapping):
-        self.R_by_time = np.sum(time_resource_matrix, 0)
-        self.R2_by_time = [ r*r for r in self.R_by_time ]
-        if np.sum(self.R2_by_time) < self.optimal_total_R_square:
+        total_R_through_time = np.sum(time_resource_matrix, 0)
+        total_R_square_through_time = [ r*r for r in total_R_through_time ]
+        if np.sum(total_R_square_through_time) < self.optimal_total_R_square:
             # print(time_resource_matrix)
-            self.optimal_total_R_square = np.sum(self.R2_by_time)
-            self.optimal_total_R = np.sum(self.R_by_time)
+            self.optimal_total_R_square = np.sum(total_R_square_through_time)
+            self.optimal_total_R = np.sum(total_R_through_time)
             self.optimal_time_resource_matrix = np.copy(time_resource_matrix)
+            self.R_by_time = np.copy(total_R_through_time)
+            self.R2_by_time = np.copy(total_R_square_through_time)
             self.update_optimal_start_and_finish_time(comb_choice, pos_in_combination_and_node_matrix_ind_mapping)
+
 
 
     def find_optimal_schedule_and_update_activity_values(self, time_resource_matrix):
@@ -88,7 +97,7 @@ class EstimatedResourceSmoothing:
                 combinations.append(schedule_options_for_this_node)
         print(combinations)
         combinations = list(itertools.product(*combinations))     
-        # 2nd Choice of Implementation --> LET'S SEE #
+        # ======  2nd Choice of Implementation --> LET'S SEE ======== #
         for comb_choice in combinations:
             for i, shift in enumerate(comb_choice):
                 node = self.node_matrix[ pos_in_combination_and_node_matrix_ind_mapping[i] ]
@@ -110,9 +119,7 @@ class EstimatedResourceSmoothing:
 
 
 
-
-
-
+## ============= Run from app.py, This main() isn't modified ============ ##
 
 def main():
     cpm = CPM()
