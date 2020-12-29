7278cc325f91022a4857880357429bd6d414c771
A. N. TANVIR <35560149+antanvir@users.noreply.github.com>
2020-12-29 18:16:54 +0600

    print details added

--- estimated_resource_smoothing.py
+++ estimated_resource_smoothing.py
@@ -20,6 +20,19 @@ class EstimatedResourceSmoothing:
         self.optimal_total_R_square = int(1e9)
     
 
+    def print_estimate_schedule_details(self):
+        print("Total R: ", self.optimal_total_R)
+        print(self.R_by_time)
+        print("Total R-square: ", self.optimal_total_R_square)
+        print(self.R2_by_time)
+        print("Name\tOS\tOF\tSlack")
+        for node in self.node_matrix:
+            if node["critical"] == True:
+                print(node["name"], "\t\t", node["ES"], "\t", node["EF"], "\t", node["slack"])
+            else:
+                print(node["name"], "\t\t", node["OS"], "\t", node["OF"], "\t", node["slack"])
+
+
     def separate_critical_activities(self):
         for node in self.node_matrix:
             if node["critical"] == True:
@@ -59,11 +72,11 @@ class EstimatedResourceSmoothing:
 
 
     def check_for_optimality(self, time_resource_matrix, comb_choice, pos_in_combination_and_node_matrix_ind_mapping):
-        total_R_through_time = np.sum(time_resource_matrix, 0)
-        total_R_square_through_time = [ r*r for r in total_R_through_time ]
-        if np.sum(total_R_square_through_time) < self.optimal_total_R_square:
-            self.optimal_total_R_square = np.sum(total_R_square_through_time)
-            self.optimal_total_R = np.sum(total_R_through_time)
+        self.R_by_time = np.sum(time_resource_matrix, 0)
+        self.R2_by_time = [ r*r for r in self.R_by_time ]
+        if np.sum(self.R2_by_time) < self.optimal_total_R_square:
+            self.optimal_total_R_square = np.sum(self.R2_by_time)
+            self.optimal_total_R = np.sum(self.R_by_time)
             self.update_optimal_start_and_finish_time(comb_choice, pos_in_combination_and_node_matrix_ind_mapping)
 
 
@@ -109,7 +122,9 @@ class EstimatedResourceSmoothing:
     def estimate_optimal_schedule(self):
         self.separate_critical_activities()
         time_resource_matrix = self.generate_time_resource_matrix()
-        # self.find_optimal_schedule_and_update_activity_values(time_resource_matrix)
+        self.find_optimal_schedule_and_update_activity_values(time_resource_matrix)
+        self.print_estimate_schedule_details()
+        
 
 
 
